74ff47c41015e03c54d0d6d5907e6fa42aced319
Gabriel <ghaabriieel@gmail.com>
2025-08-13 11:26:54 -0300

    Feat: Adiconando modal aos cards de projeto ainda sem estilização e conteudo

--- src/componentsPage/card/CardFull.tsx
+++ src/componentsPage/card/CardFull.tsx
@@ -1,6 +1,6 @@
 //Importação chakra ui
-import { Flex, Stack, Tag, } from "@chakra-ui/react"
-
+import { Flex, Stack, Tag, Button, CloseButton, Dialog, Portal } from "@chakra-ui/react"
+import { useState } from "react"
 //Importação de componentes
 import TitleDescriptionCard from "@/componentsPage/card/TitleDescriptionCard"
 import IconsDescription from "@/componentsPage/card/IconsDescription"
@@ -16,33 +16,65 @@ interface Props {
     iconDescriptionTechnologies: TechnologyKey[]
     titleDescription: string
     textDescription: string
-     defaultText?: string
+    defaultText?: string
 
 }
 
-function CardFull({ imgCard, iconDescriptionTechnologies, titleDescription, textDescription,  defaultText = "Projeto"}: Props) {
+function CardFull({ imgCard, iconDescriptionTechnologies, titleDescription, textDescription, defaultText = "Projeto" }: Props) {
+    const [open, setOpen] = useState(false)
     return (
-        <Flex bg="bg.muted" direction="column" minW={{ base: "220px", md: "290px" }} flex="1" borderRadius="lg" shadow="sm" transition="all 0.3s ease"
-            _hover={{
-                transform: "translateY(-4px) rotate(-0.5deg)",
-                boxShadow: "xl",
-            }}
-            _active={{
-                transform: "translateY(-4px) rotate(-0.5deg)",
-                boxShadow: "xl",
-            }}
-            
-            
-            >
-            <ImgContain imagem={imgCard} />
-            <Tag.Root bg="purple.emphasized" color="white" size="lg" variant="solid" w="fit-content" borderBottomRadius={"md"}  borderTopRadius="none" >
-                <Tag.Label>{defaultText} </Tag.Label>
-            </Tag.Root>
-            <Stack gap="2" p="4" borderBottomRadius="lg" >
-                <TitleDescriptionCard title={titleDescription} text={textDescription} />
-                <IconsDescription  technologies={iconDescriptionTechnologies} />
-            </Stack>
-        </Flex>
+        <>
+            <Dialog.Root lazyMount open={open} onOpenChange={(e) => setOpen(e.open)}  placement={"center"}>
+                <Dialog.Trigger asChild>
+                    <Flex bg="bg.muted" direction="column" minW={{ base: "220px", md: "290px" }} flex="1" borderRadius="lg" shadow="sm" transition="all 0.3s ease"
+                        _hover={{
+                            transform: "translateY(-4px) rotate(-0.5deg)",
+                            boxShadow: "xl",
+                        }}
+                        _active={{
+                            transform: "translateY(-4px) rotate(-0.5deg)",
+                            boxShadow: "xl",
+                        }}
+                    >
+
+                        <ImgContain imagem={imgCard} />
+                        <Tag.Root bg="purple.emphasized" color="white" size="lg" variant="solid" w="fit-content" borderBottomRadius={"md"} borderTopRadius="none" >
+                            <Tag.Label>{defaultText} </Tag.Label>
+
+                        </Tag.Root>
+
+                        <Stack gap="2" p="4" borderBottomRadius="lg" >
+                            <TitleDescriptionCard title={titleDescription} text={textDescription} />
+                            <IconsDescription technologies={iconDescriptionTechnologies} />
+                        </Stack>
+
+                    </Flex>
+                </Dialog.Trigger>
+                <Portal>
+                    <Dialog.Backdrop />
+                    <Dialog.Positioner>
+                        <Dialog.Content>
+                            <Dialog.Header>
+                                <Dialog.Title>Dialog Title</Dialog.Title>
+                            </Dialog.Header>
+                            <Dialog.Body>
+
+                            </Dialog.Body>
+                            <Dialog.Footer>
+                                <Dialog.ActionTrigger asChild>
+                                    <Button variant="outline">Cancel</Button>
+                                </Dialog.ActionTrigger>
+                                <Button>Save</Button>
+                            </Dialog.Footer>
+                            <Dialog.CloseTrigger asChild>
+                                <CloseButton size="sm" />
+                            </Dialog.CloseTrigger>
+                        </Dialog.Content>
+                    </Dialog.Positioner>
+                </Portal>
+            </Dialog.Root>
+        </>
+
     )
 }
 
